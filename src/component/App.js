72ff975029c7ab9dce3508507e6e1fc321fa0bb1
Polashahmad01 <dev.polashahmad@gmai.com>
2020-10-31 21:47:24 +0600

    complete

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import questions from './data';
+
+import data from './data';
+import Question from './Questions';
 
 const App = () => {
+    const [ question, setQuestion ] = useState(data);
 
-    return <div>App</div>
+    return (
+        <main>
+            <div className="container">
+                <h3>Arrordion App</h3>
+                <section className="info">
+                    {question.map(singleQuestion => {
+                        return <Question key={singleQuestion.id} {...singleQuestion} />
+                    })}
+                </section>
+            </div>
+        </main>
+    );
 };
 
 export default App;
--- src/component/Questions.js
+++ src/component/Questions.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const Question = () => {
+const Question = ({ title, info }) => {
+    const [showInfo, setShowInfo ] = useState(false);
 
-    return <div>Question</div>
+    return (
+        <article className="question">
+            <header>
+                <h4>{title}</h4>
+                <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus /> }
+                </button>
+            </header>
+            {showInfo && <p>{info}</p>}
+        </article>
+    );
 };
 
 export default Question;
